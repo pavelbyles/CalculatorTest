713b97b1d721bc02616baa0f166bf41c901cac39
Pavel Byles <pavelbyles@gmail.com>
2017-10-10 22:13:22 -0500

    Still testing logging

--- cloudfunctions/function.js
+++ cloudfunctions/function.js
@@ -8,7 +8,7 @@ exports.subscribe = function (event, callback) {
   const pubsubMessage = event.data;
   const name = pubsubMessage.data ? Buffer.from(pubsubMessage.data, 'base64').toString() : 'World';
 
-  console.log('Hello, %s', name);
+  console.log(`Hello, ${name}!`);
 
   callback();
 };
