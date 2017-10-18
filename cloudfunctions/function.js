bed7b8493e24424abc6ee81f60cb305034add646
Pavel Byles <pavelbyles@gmail.com>
2017-10-18 14:50:25 -0500

    Update function.js

--- cloudfunctions/function.js
+++ cloudfunctions/function.js
@@ -7,6 +7,7 @@
 exports.writeCalcResult = function (event, callback) {
   const pubsubMessage = event.data;
   const content = pubsubMessage.data ? Buffer.from(pubsubMessage.data, 'base64').toString() : '{"name":"World"}';
+  console.log('Input is, %s', content);
   const parsedContent = JSON.parse(content)
   console.log('Hello, %s', parsedContent.name);
 
